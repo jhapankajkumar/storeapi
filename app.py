31cdd300bd7a28694c48fb27b6622ca27aec8a34
pankaj kumar jha <jha.pankaj55@gmail.com>
2020-03-28 02:03:01 +0800

    Modified app.py

--- app.py
+++ app.py
@@ -11,15 +11,12 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 
+app.config['DEBUG'] = True
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['DEBUG'] = True
-api = Api(app)
 app.secret_key = 'pankaj'  # This must be long and unique
-
-
-
+api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 

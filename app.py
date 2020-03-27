b282284db1c16ccaec75af0e5db470ad88858344
pankaj kumar jha <jha.pankaj55@gmail.com>
2020-03-28 00:28:27 +0800

    Added gitignore

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,17 +10,16 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['DEBUG'] = True
 api = Api(app)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 app.secret_key = 'pankaj'  # This must be long and unique
+
+
+
 
 jwt = JWT(app, authenticate, identity)
 
@@ -30,12 +31,18 @@ def home():
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
+api.add_resource(UserRegister, '/register')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(debug=True)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
